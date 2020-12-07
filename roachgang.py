5188786c3f0edfab54f3c3aa9d3233e6f7425739
Luis <39094995+lgon070@users.noreply.github.com>
2020-12-07 15:36:23 -0800

    Add files via upload

---
+++ roachgang.py
@@ -0,0 +1,25 @@
+text = input("Text to translate to Roachanese: ")
+words = text.split(" ")
+special_chars = [".", "?", ",", ";", ":", "!"]
+translated = []
+
+for word in words:
+    if "brother" in word:
+        translated.append(word)
+    else:
+        newWord = word
+        noSC = True
+        for char in special_chars:
+            if char in word:
+                noSC = False
+                index = word.find(char)
+                newWord = word[:index] + "roach" + word[index:]
+        if noSC:
+            translated.append(newWord + "roach")
+        else:
+            translated.append(newWord)
+
+translatedText = " "
+print(translatedText.join(translated))
+
+
